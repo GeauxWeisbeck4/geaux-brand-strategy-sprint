6e91dbb4434880ae7fb9b1ebd431002f06f8cdc4
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2022-09-21 12:01:22 -0400

    Update and switch dev computer

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tinyrocket.pages.dev',
+  site: 'https://geauxbrandstrategy.netlify.app',
   integrations: [preact(), tailwind(), sitemap(), mdx()],
   legacy: {
     astroFlavoredMarkdown: true
