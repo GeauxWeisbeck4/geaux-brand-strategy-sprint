4a1ae4a388f091d487b75deafcf12cca15fcb686
Andrew <andrew.weisbeck@gmail.com>
2022-09-23 02:50:00 -0400

    Committing Updated Home page
    Added navbar, set font and theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,22 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 import mdx from "@astrojs/mdx";
+import storyblok from "@storyblok/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://geauxbrandstrategy.netlify.app',
-  integrations: [preact(), tailwind(), sitemap(), mdx()],
+  integrations: [preact(), tailwind(), sitemap(), mdx(),
+    storyblok({
+      accessToken: "<your-access-token>",
+      components: {
+        page: "storyblok/Page",
+        baseHead: "storyblok/BaseHead",
+        blogHeader: "storyblok/BlogHeader",
+        blotPostPriview: "storyblok/BlogPostPreview"
+      }
+    })
+  ],
   legacy: {
     astroFlavoredMarkdown: true
   }
